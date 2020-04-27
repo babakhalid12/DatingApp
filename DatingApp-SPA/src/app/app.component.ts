4c49b32a7b571f9e7c7b3c8ed064c350811f35a3
unknown <babakhalid.patel@gmail.com>
2020-04-27 14:19:03 +0530

    Initial Commit

---
+++ DatingApp-SPA/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {Http} from '@angular/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+
+  constructor(private http: Http) {
+
+  }
+
+// tslint:disable-next-line:eofline
+}
---
+++ DatingApp-SPA/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
+import { ValueComponent } from './value/value.component';
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      ValueComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
